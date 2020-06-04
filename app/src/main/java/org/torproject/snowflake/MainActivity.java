cfbf2d3f4863f591d451303820476a0d5fac6f4b
Hashik Donthineni <HashikDonthineni@gmail.com>
2020-06-05 02:18:34 +0530

    Added bare service class and called it from MainActivity

--- app/src/main/java/org/torproject/snowflake/MainActivity.java
+++ app/src/main/java/org/torproject/snowflake/MainActivity.java
@@ -1,44 +1,60 @@
 package org.torproject.snowflake;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ActivityManager;
-import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.torproject.snowflake.constants.ForegroundServiceConstants;
+
+/**
+ * MainActivity is the main UI of the application.
+ */
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPreferences = getSharedPreferences(getString(R.string.sharedpreference_file), MODE_PRIVATE);
 
         Button startButton = findViewById(R.id.start_button);
-
         startButton.setOnClickListener(v -> {
-            if (isServiceRunning()){ //Toggling the service.
-                //TODO: Start service
-            }
-            else{
-                //TODO: Stop service
-            }
+            if (isServiceRunning()) //Toggling the service.
+                serviceToggle(ForegroundServiceConstants.ACTION_STOP);
+            else
+                serviceToggle(ForegroundServiceConstants.ACTION_START);
         });
         if (BuildConfig.DEBUG)
             startButton.performClick(); //To perform an automatic click in testing environment.
     }
 
-    //Test to see if the service is already running.
-    private boolean isServiceRunning() {
-        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        if (manager != null) {
-            for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-                if (MyPersistentService.class.getName().equals(service.service.getClassName())) {
-                    return true;
-                }
-            }
+    /**
+     * Turn service on/off.
+     *
+     * @param action An Action from ForegroundServiceConstants.
+     */
+    private void serviceToggle(String action) {
+        Intent serviceIntent = new Intent(MainActivity.this, MyPersistentService.class);
+        serviceIntent.setAction(action);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent);
+        } else {
+            startService(serviceIntent);
         }
-        return false;
+    }
+
+    /**
+     * Test to see if the MyPersistentService is running or not.
+     *
+     * @return boolean whether the service is running or not.
+     */
+    private boolean isServiceRunning() {
+        return sharedPreferences.getBoolean(getString(R.string.is_service_running_bool), false);
     }
 }
---
+++ app/src/main/java/org/torproject/snowflake/constants/ForegroundServiceConstants.java
@@ -0,0 +1,10 @@
+package org.torproject.snowflake.constants;
+
+public class ForegroundServiceConstants {
+    public static final String ACTION_START = "1";
+    public static final String ACTION_STOP = "0";
+
+    //Shared Preferences State
+    public static final int SERVICE_RUNNING = 1;
+    public static final int SERVICE_STOPPED = 0;
+}
