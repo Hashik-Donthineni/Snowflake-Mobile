74623649a66c6e4fdd7c405b313439fbb4755fe9
Hashik Donthineni <HashikDonthineni@gmail.com>
2020-06-17 13:25:48 +0530

    Removed hardcoded headers from service and added them dynamically

--- app/src/main/java/org/torproject/snowflake/services/SendAnswerService.java
+++ app/src/main/java/org/torproject/snowflake/services/SendAnswerService.java
@@ -3,13 +3,17 @@ package org.torproject.snowflake.services;
 import org.torproject.snowflake.pojo.AnsResponse;
 import org.torproject.snowflake.pojo.AnswerBody;
 
+import java.util.Map;
+
 import io.reactivex.rxjava3.core.Observable;
 import retrofit2.http.Body;
+import retrofit2.http.HeaderMap;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface SendAnswerService {
     @POST("answer")
-    @Headers({"Content-type: application/json", "Host: 10.0.2.2:8080"})
-    Observable<AnsResponse> sendAnswer(@Body AnswerBody body);
+    Observable<AnsResponse> sendAnswer(
+            @HeaderMap Map<String, String> headersMap,
+            @Body AnswerBody body);
 }
