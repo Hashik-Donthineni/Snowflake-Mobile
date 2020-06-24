4ff7285c94411e5c14b09fc0a2d06f7fc592ecfc
Hashik Donthineni <HashikDonthineni@gmail.com>
2020-06-24 14:32:17 +0530

    Added Socket URL to GlobalApplication

--- app/src/main/java/org/torproject/snowflake/GlobalApplication.java
+++ app/src/main/java/org/torproject/snowflake/GlobalApplication.java
@@ -7,9 +7,14 @@ import java.util.Map;
 
 public class GlobalApplication extends Application {
     private final static String BROKER_URL = "http://10.0.2.2:8080"; //10.0.2.2 is used to access computer's local host from Android Emulator.
+    private final static String WEBSOCKET_URL = "wss://snowflake.freehaven.net:443";
 
     public static String getBrokerUrl() {
         return BROKER_URL;
+    }
+
+    public static String getWebSocketUrl() {
+        return WEBSOCKET_URL;
     }
 
     public static Map<String, String> getHeadersMap() {
