a9548589575a7bc6dc443df09189e5eef27b4c1c
Hashik Donthineni <HashikDonthineni@gmail.com>
2020-06-28 16:19:23 +0530

    Handled back press from fragments

---
+++ app/src/main/java/org/torproject/snowflake/constants/FragmentConstants.java
@@ -0,0 +1,6 @@
+package org.torproject.snowflake.constants;
+
+public class FragmentConstants {
+    public static final int MAIN_FRAGMENT = 0;
+    public static final int APP_SETTINGS_FRAGMENT = 1;
+}
