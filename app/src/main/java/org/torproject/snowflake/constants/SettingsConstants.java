5078bbb0325b25c01c58bbf7a282f6218fd9ddb1
Hashik Donthineni <HashikDonthineni@gmail.com>
2020-06-28 18:46:14 +0530

    Fixed the bug of EditText deactivated when app restarts

---
+++ app/src/main/java/org/torproject/snowflake/constants/SettingsConstants.java
@@ -0,0 +1,24 @@
+package org.torproject.snowflake.constants;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SettingsConstants {
+    //Switches
+    public static final String STUN_SWITCH = "stun_switch";
+    public static final String BROKER_SWITCH = "broker_switch";
+    public static final String RELAY_SWITCH = "relay_switch";
+
+    //Edit Texts
+    public static final String STUN_ET = "stun_edit_text";
+    public static final String BROKER_ET = "broker_edit_text";
+    public static final String RELAY_ET = "relay_edit_text";
+
+    public static Map<String, String> getSettingMap(){
+        return new HashMap<String, String>(){{
+            put(STUN_SWITCH, STUN_ET);
+            put(BROKER_SWITCH, BROKER_ET);
+            put(RELAY_SWITCH, RELAY_ET);
+        }};
+    }
+}
